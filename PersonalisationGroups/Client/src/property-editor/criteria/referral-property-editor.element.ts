0f40f0a9959c9947b04a4416d10cd3478092b074
Andy Butland <abutland73@gmail.com>
2024-08-08 09:45:00 +0200

    Editor and translator for referral criteria.

--- PersonalisationGroups/Client/src/property-editor/criteria/referral-property-editor.element.ts
+++ PersonalisationGroups/Client/src/property-editor/criteria/referral-property-editor.element.ts
@@ -2,9 +2,17 @@ import {
   html,
   customElement,
   property,
+  state,
+  css,
 } from "@umbraco-cms/backoffice/external/lit";
 import type { UmbPropertyEditorUiElement } from "@umbraco-cms/backoffice/extension-registry";
 import { UmbLitElement } from "@umbraco-cms/backoffice/lit-element";
+import { UUIInputEvent, UUISelectEvent } from "@umbraco-cms/backoffice/external/uui";
+
+type ReferralSetting = {
+  match: string;
+  value: string
+};
 
 const elementName = "personalisation-group-referral-criteria-property-editor";
 
@@ -15,7 +23,10 @@ export class ReferralCriteriaPropertyUiElement extends UmbLitElement implements 
   @property({ type: String })
   set value(value: string) {
       this.#value = value;
-      // TODO: To local value.
+      if (value.length > 0) {
+        this._typedValue = JSON.parse(value);
+      }
+
       this.requestUpdate();
   }
 
@@ -23,16 +34,87 @@ export class ReferralCriteriaPropertyUiElement extends UmbLitElement implements 
       return this.#value;
   }
 
-  // TODO: From local value
-  // #refreshValue() {
-  //   this.dispatchEvent(
-  //     new CustomEvent("change", { composed: true, bubbles: true })
-  //   );
-  // }
+  @state()
+  private _typedValue: ReferralSetting = { match: "MatchesValue", value: "" };
+
+  #getMatchOptions() {
+    return [{
+        name: "Matches value",
+        value: "MatchesValue",
+        selected: this._typedValue.match === "MatchesValue",
+    },
+    {
+        name: "Does not match value",
+        value: "DoesNotMatchValue",
+        selected: this._typedValue.match === "DoesNotMatchValue",
+    },
+    {
+        name: "Contains value",
+        value: "ContainsValue",
+        selected: this._typedValue.match === "ContainsValue",
+    },
+    {
+        name: "Does not contain value",
+        value: "DoesNotContainValue",
+        selected: this._typedValue.match === "DoesNotContainValue",
+    }];
+  }
+
+  #onMatchChange(e: UUISelectEvent) {
+    this._typedValue.match = e.target.value.toString();
+    this.#refreshValue();
+  }
+
+  #onValueChange(e: UUIInputEvent) {
+    this._typedValue.value = e.target.value.toString();
+    this.#refreshValue();
+  }
+
+  #refreshValue() {
+    this.#value = JSON.stringify(this._typedValue)
+    this.dispatchEvent(
+      new CustomEvent("change", { composed: true, bubbles: true })
+    );
+  }
 
   render() {
-    return html``;
+    return html`
+      <p>Please enter the host settings:</p>
+      <table>
+        <tr>
+          <td class="label"><label for="Match">Comparison:</label></td>
+          <td>
+              <uui-select
+                  id="Match"
+                  label="Match"
+                  @change=${this.#onMatchChange}
+                  .options=${this.#getMatchOptions()}
+              ></uui-select>
+          </td>
+        </tr>
+        <tr>
+          <td class="label"><label for="Value">Value:</label></td>
+          <td>
+            <uui-input
+                id="Value"
+                label="Value"
+                .value=${this._typedValue.value}
+                @change=${this.#onValueChange}>
+              </uui-input>
+          </td>
+        </tr>
+    </table>`;
   }
+
+  static styles = [
+    css`
+      td.label {
+        vertical-align: top;
+        padding-top: 4px;
+        padding-right: 4px;
+      }
+    `,
+  ];
 }
 
 export default ReferralCriteriaPropertyUiElement;
--- PersonalisationGroups/Client/src/translator/referral-definition-detail.translator.ts
+++ PersonalisationGroups/Client/src/translator/referral-definition-detail.translator.ts
@@ -3,8 +3,27 @@
 export class ReferralDefinitionDetailTranslator implements PersonalisationGroupDefinitionDetailTranslatorApi  {
    translate(definition: string) {
 		console.log(definition);
-		let translation = "";
-		return translation;
+    let translation = "";
+    if (definition) {
+        const selectedReferrerDetails = JSON.parse(definition);
+        translation = "Referrer value ";
+        switch (selectedReferrerDetails.match) {
+            case "MatchesValue":
+                translation += "matches '" + selectedReferrerDetails.value + "'.";
+                break;
+            case "DoesNotMatchValue":
+                translation += "does not match '" + selectedReferrerDetails.value + "'.";
+                break;
+            case "ContainsValue":
+                translation += "contains '" + selectedReferrerDetails.value + "'.";
+                break;
+            case "DoesNotContainValue":
+                translation += "does not contain '" + selectedReferrerDetails.value + "'.";
+                break;
+        }
+    }
+
+    return translation;
   }
 
   destroy() {
